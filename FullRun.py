472a8b69bbea339921132388b9c33068d0005f3f
SuperCode10 <rishrao987@outlook.com>
2022-09-03 13:34:39 -0700

    Mostly working drive Straight.

--- FullRun.py
+++ FullRun.py
@@ -1,13 +1,12 @@
-# from typing import final
-# from audioop import mul
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
-from spike import PrimeHub, MotionSensor, ColorSensor, Motor, MotorPair
-import time
+import time, hub
 from math import *
 
-hub = PrimeHub()
+WHEEL_RADIUS_CM = 4.3
+GLOBAL_LEVEL = 4
+primeHub = PrimeHub()
 
 motorC = Motor("C")
 motorE = Motor("E")
@@ -19,31 +18,27 @@ motorF = Motor("F")
 _CM_PER_INCH = 2.54
 
 def initialize():
-    motors.set_stop_action("hold")
-    motors.set_motor_rotation(27.6, 'cm')
+    print("___________________________________________________")
+    primeHub.motion_sensor.reset_yaw_angle()
+    motors.set_stop_action("brake")
+    motors.set_motor_rotation(2*3.14*WHEEL_RADIUS_CM, 'cm')
+    isBatteryGood()
 
-#Utilities
-def turn(angle = 0, leftSpeed = 0, rightSpeed = 0):
+# -------------------------------------------------------------------  Utilities --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def logMessage(message = "", level=1):
     """
-    Turn Information:
-    - Purpose - Turn turns the drivebase to the specified angle
-    - Parameters: Angle(Int), leftSpeed(Int), rightSpeed(Int)
-    - Issues: Off by ~5 degrees
-    - Optimal Speed: 25
+    level: parameter between 1-5. 5 is the most detailed level.
+
+    Prints the message that is passed to the function
+    The printing is controlled by the level parameter.
+    The function will only print if the passed level is higher than the global level.
+    If the global level is set to zero nothing will print.
     """
+    if level == 0:
+        level = 1
 
-    '''this goes on the start of the program'''
-    #motors.set_stop_action("hold")
-    #motors.set_motor_rotation(27.6, 'cm')
-
-    hub.motion_sensor.reset_yaw_angle()
-    startAngle = hub.motion_sensor.get_yaw_angle()
-    currentAngle = hub.motion_sensor.get_yaw_angle()
-    motors.start_tank_at_power(leftSpeed, rightSpeed)
-    while abs(hub.motion_sensor.get_yaw_angle()) - abs(startAngle) + 15 < abs(angle):
-        currentAngle = hub.motion_sensor.get_yaw_angle()
-        print("Distance turned: " + str(abs(currentAngle) - abs(startAngle)))
-    motors.stop()
+    if (level <= GLOBAL_LEVEL):
+        print(message)
 
 def moveArm(degrees = 0, speed = 0, motor = motorD):
     """
@@ -57,28 +52,24 @@ def moveArm(degrees = 0, speed = 0, motor = motorD):
     motor.start_at_power(speed)
     while abs(currentDegrees) - abs(startDegrees) < abs(degrees):
         currentDegrees = motor.get_degrees_counted()
-        print("MotorD smaller: " + str(abs(currentDegrees) - abs(startDegrees)))
+        #print("MotorD smaller: " + str(abs(currentDegrees) - abs(startDegrees)))
     motor.stop()
 
 def goF(distancein, speed,):
     motors.move_tank(distancein, 'in', speed, speed)
-
-def energyStorage():
     goF(20, 30)
-    turn(95, 25, -25)
+    #turn(95, 25, -25)
     goF(6, 30)
-    turn(95, -25, 25)
+    #turn(95, -25, 25)
     goF(10, 30)
     moveArm(1000, 30, motorD)
-
-#energyStorage()
 
 def turnToAngle(targetAngle = 0, speed = 25, forceTurn = "None", slowTurnRatio = 0.4):
     # Initialize function
     motors.stop()
-    currentAngle = hub.motion_sensor.get_yaw_angle()
-    print("TurnToAngle current angle is " + str(currentAngle) + " and target angle is" + str(targetAngle))
-
+    currentAngle = primeHub.motion_sensor.get_yaw_angle()
+    #print("TurnToAngle current angle is " + str(currentAngle) + " and target angle is" + str(targetAngle))
+    
     # Make the target and current angles positive if they are not already
     if currentAngle < 0:
         currentAngle = currentAngle + 360
@@ -111,7 +102,7 @@ def turnToAngle(targetAngle = 0, speed = 25, forceTurn = "None", slowTurnRatio =
     elif forceTurn == "Left":
         degreesToTurn = degreesToTurnLeft * -1
 
-    print("TurnToAngle degrees to turn is " + str(degreesToTurn))
+    #print("TurnToAngle degrees to turn is " + str(degreesToTurn))
 
     # Call the internal function that actually turns the robot
     _turnRobot(degreesToTurn, speed, slowTurnRatio)
@@ -120,9 +111,9 @@ def _turnRobot(angleInDegrees, speed, slowTurnRatio):
     # Initialize the function
     motors.stop()
     slowTurnDegrees = slowTurnRatio * abs(angleInDegrees)
-    initialAngle = hub.motion_sensor.get_yaw_angle()
+    initialAngle = primeHub.motion_sensor.get_yaw_angle()
     finalAngle = initialAngle + angleInDegrees
-    print("TurnToAngle initial gyro angle is " + str(initialAngle) + " and target angle is " + str(finalAngle))
+    #print("TurnToAngle initial gyro angle is " + str(initialAngle) + " and target angle is " + str(finalAngle))
     initialTurn = 0
 
     # Calculate the initial turn distance
@@ -133,13 +124,13 @@ def _turnRobot(angleInDegrees, speed, slowTurnRatio):
 
     # Turn the initial amount
     motors.move_tank(initialTurn, "degrees", speed, -1 * speed)
-    motors.stop()
+    #motors.stop() #Commented out to stop jerking between 2 parts of turn
 
     # Initialize slow turn
     initialSlowSpeed = 5
 
-    robotAngle = hub.motion_sensor.get_yaw_angle()
-    print("TurnToAngle gyro angle before slow turn is " + str(robotAngle))
+    robotAngle = primeHub.motion_sensor.get_yaw_angle()
+    #print("TurnToAngle gyro angle before slow turn is " + str(robotAngle))
 
     # Turn slowly
     while abs(robotAngle - finalAngle) > 1:
@@ -147,46 +138,37 @@ def _turnRobot(angleInDegrees, speed, slowTurnRatio):
             motors.start_tank(initialSlowSpeed * -1, initialSlowSpeed)
         else:
             motors.start_tank(initialSlowSpeed, initialSlowSpeed * -1)
-        robotAngle = hub.motion_sensor.get_yaw_angle()
+        robotAngle = primeHub.motion_sensor.get_yaw_angle()
         
 
     motors.stop()
 
-    print("TurnToAngle final robot angle is " + str(hub.motion_sensor.get_yaw_angle()))
-
 def testTurnToAngle():
     # TurnToAngle Testing
-    hub.motion_sensor.reset_yaw_angle()
+    primeHub.motion_sensor.reset_yaw_angle()
     multiplier = 1
-    print("____________________________________________")
     start = time.ticks_us()
     while multiplier < 5:
         turnToAngle(90 * multiplier, 25)
         multiplier = multiplier + 1
         time.sleep(1)
     time.sleep(1)        
-    print("Final Angle is " + str(hub.motion_sensor.get_yaw_angle()))
+    print("Final Angle is " + str(primeHub.motion_sensor.get_yaw_angle()))
     end = time.ticks_us()
     print("Total time = " + str(end - start))
 
-
-
-def gyroStraight(targetAngle, distance, speed = 20, backward = False):
-    wheelDiameter = 8.8 # could be 8.8
-    degreesToCover = (distance * 360)/(wheelDiameter * 3.14)
-    #print(degreesToCover)
-    position_start = motorE.get_degrees_counted();
-    #print("Position Start = " + str(position_start))
+def gyroStraight(distance, speed = 20, backward = False, targetAngle = 0):
+    print("GYROSTRAIGHT START: targetAngle  is " + str(targetAngle))
+    degreesToCover = (distance * 360)/(WHEEL_RADIUS_CM * 2 * 3.14)
+    position_start = motorE.get_degrees_counted()
     if (backward): 
-        while ((motorE.get_degrees_counted() - position_start)  > degreesToCover * -1  ):
-            #print("degrees = " + str(right_large_motor.get_degrees_counted() - position_start))
-            currentAngle = hub.motion_sensor.get_yaw_angle()
+        while ((motorE.get_degrees_counted() - position_start)  > degreesToCover * -1):
+            currentAngle = primeHub.motion_sensor.get_yaw_angle()
             correction = targetAngle - currentAngle
-            motors.start(correction, speed * -1  )
+            motors.start(correction, speed * -1)
     else:
-         while ((motorE.get_degrees_counted() - position_start)  < degreesToCover  ):
-           # print("degrees = " + str(right_large_motor.get_degrees_counted() - position_start))
-            currentAngle = hub.motion_sensor.get_yaw_angle()
+         while ((motorE.get_degrees_counted() - position_start)  < degreesToCover):
+            currentAngle = primeHub.motion_sensor.get_yaw_angle()
             correction = targetAngle - currentAngle
             motors.start(correction, speed)
 
@@ -200,6 +182,132 @@ def testGyro():
     gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*2)
     gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*3, backward=True)
 
+def convertDegToCM(degrees):
+    return degrees * WHEEL_RADIUS_CM * pi * 2 / 360
+
+def converCMToDeg(distance):
+    return distance * 360 / (WHEEL_RADIUS_CM * pi * 2)
+
+def convertInchesToCM(distanceInInches):
+    """
+    Convert Inches To CM
+    ____________________
+    """
+    return _CM_PER_INCH * distanceInInches
+
+def drive(speed, distanceInCM, target_angle, gain = 1):
+    """
+    Drive
+    _____
+    This function drives the robot FORWARD using the motion sensor and the 80-15-5 formula.
+    80% of distance at speed
+    15% of distance with linear slow down from speed to 10
+    5% of distance with speed of FINAL_SLOW_SPEED.
+    _____
+    Speed - Speed the motors travel at. Integer from -100 to 100
+    DistanceInCM - Distance to travel in centimeters. Integer greater than 0
+    TargetAngle - The angle the robot should drive at. Integer from 0 to 360
+    Gain - The multiplier off the error. Integer greater than 0
+    """
+    motors.stop()
+    logMessage("driveStraight for distance: " + str(distanceInCM) + " and target angle" + str(target_angle), level=4)
+    initialDeg = abs(motorC.get_degrees_counted())
+
+    # If the distance is small, then just drive over that distance at speed.
+    if (distanceInCM < 5):
+        _driveStraightWithSlowDown(distanceInCM, speed, target_angle, gain, slowDown=False)    
+
+    # First drive 80% of the distance at
+    distance80 = distanceInCM * 1.0
+    _driveStraightWithSlowDown(distance80, speed, target_angle, gain, slowDown=False)
+
+    # This code is commented for now, the above code seems to be providing the best algorithm
+    # We will reevaluate if needed.
+    '''
+    # Drive the next 16% of the distance at a speed that reduces from speed to speed=FINAL_SLOW_SPEED
+    distanceTravelled = convertDegToCM(abs(motorC.get_degrees_counted()) - initialDeg)
+    remainingDistance = distanceInCM - distanceTravelled
+    logMessage("Distance travelled after first part = "  + str(distanceTravelled) + " error=" + str(distanceTravelled-distance80), level=4)
+
+    distance16 = remainingDistance * 0.8
+    _driveStraightWithSlowDown(distance16, speed, target_angle, gain, slowDown=True)
+
+    distanceTravelled = convertDegToCM(abs(motorC.get_degrees_counted()) - initialDeg)
+    remainingDistance = distanceInCM - distanceTravelled
+    logMessage("Distance travelled after second part= "  + str(distanceTravelled) + " error=" + str(distanceTravelled - (distance80+distance16)), level=4)
+
+    # Drive the final 4% of the distance at speed 5
+    FINAL_SLOW_SPEED = 5
+    motors.move_tank(remainingDistance, "cm", FINAL_SLOW_SPEED, FINAL_SLOW_SPEED)
+    '''
+    motors.stop()
+    finalDeg = abs(motorC.get_degrees_counted())
+
+    totalDistanceTravelled = convertDegToCM(finalDeg - initialDeg)
+    logMessage("Total distance travelled = " + str(totalDistanceTravelled) + " error=" + str(distanceInCM-totalDistanceTravelled), level=4)
+
+def _driveStraightWithSlowDown(distance, speed, target_angle, gain, slowDown):
+    """
+    Drive Straight
+    ______________
+    This is a internal function do not call directly. Call drive instead.
+
+    The algorithm goes from speed to speed 10 until distance is travelled.
+    slowDown: True if you want to slow down over the distance. 
+    """
+    startDistanceInDeg = abs(motorC.get_degrees_counted())
+    logMessage("startDistanceInDeg = " + str(int(startDistanceInDeg)), level=5)
+    distanceInDeg = converCMToDeg(distance)
+    currentSpeed = speed
+
+    # Drop the speed from speed to five in distanceInDeg.
+    distanceInDegTravelled = 0
+    while  distanceInDegTravelled <= distanceInDeg:
+        if (slowDown == True):
+            currentSpeed = (distanceInDegTravelled * (10 - speed) / (distanceInDeg)) + speed
+        
+        correction = target_angle - primeHub.motion_sensor.get_yaw_angle()
+        turn_rate = correction * gain
+        logMessage("currentSpeed = " + str(int(currentSpeed)) + " distanceInDegTravelled = " + str(distanceInDegTravelled) + " distanceInDeg=" + str(distanceInDeg), level=5)
+        motors.start(turn_rate, int(currentSpeed))
+        distanceInDegTravelled = abs(motorC.get_degrees_counted()) - startDistanceInDeg
+    logMessage("Drivestraight completed", level=5)
+
+def isGyroGood():
+    """
+    Is Gyro Good
+    ____________
+    This function checks if the gyro is drifting or not. If it is not, then is returns true.
+    Otherwise, it returns false and plays a beep noise.
+    ____________
+    It is recommended to use this function BEFORE the match, as it takes ~ 6 seconds to complete.
+    """
+    angle1 = primeHub.motion_sensor.get_yaw_angle()
+    time.sleep(5)
+    angle2 = primeHub.motion_sensor.get_yaw_angle()
+
+    if abs(angle1 - angle2) > 2:
+        primeHub.speaker.beep(60, 1)
+        print("Please reset gyro")
+        return False
+    
+    return True
+
+def isBatteryGood():
+    logMessage("Battery voldate: " + str(hub.battery.voltage()), level=1)
+    
+    if hub.battery.voltage() < 7600:
+        primeHub.speaker.beep(120, 1)
+        print("Please recharge robot")
+        return False
+
+    return True
+ 
+ # ------------------------------------------------------------------- End Utilities --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+def run4():
+    drive(speed = 30, distanceInCM = 20, target_angle = 0)
 
 initialize()
-testTurnToAngle()
+run4()
+raise SystemExit
