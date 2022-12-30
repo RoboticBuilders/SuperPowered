2ff38f759ecae3e194c94adb22db03914d49dea9
RoboticBuilders <rishrao987@gmail.com>
2022-12-29 16:32:48 -0800

    Creating cleanedup FullRunWithFewerArms

--- testMethods.py
+++ testMethods.py
@@ -30,45 +30,39 @@ def squareTest():
     turnToAngle(targetAngle = -180, speed = 30)
     drive(speed = 30, distanceInCM = 30, target_angle = -180)
 
+def testGyro():
+    wheels.move(amount = 29, unit = "in", steering = 0, speed = 60)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*16)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*2, backward =True)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*2)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*2, backward=True)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*2)
+    # gyroStraight(targetAngle = 0,  distance = _CM_PER_INCH*3, backward=True)
 
-def testTurnToAngle():
-    # TurnToAngle Testing
-    
-    turnRobotRelative(degreesToTurn=90, speed=25, slowTurnRatio = 0.2, correction = 0.07)
-    #turnRobotRelative(degreesToTurn=90, speed=25, slowTurnRatio = 0.4, correction = 0.05)
-    #turnRobotRelative(degreesToTurn=90, speed=25, slowTurnRatio = 0.4, correction = 0.05)
-    #turnToAngle(targetAngle = 90, speed = 25, forceTurn = "None", slowTurnRatio = 0.4, correction = 0.05)
 
-    """
-    # Right turn non zero crossing
-    turnToAngle(targetAngle = 45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
+def testTurnToAngle(speed=20, slowturnratio = 0.4, correction=0.16):
+    random.seed(5)
+    targetAngle = -30
+    increment = -30
+    while(True):
+        #targetAngle = random.randint(-178, 0)
+        print("Asking turnToAngle to turn to " + str(targetAngle))
+        #newturnToAngle(targetAngle=targetAngle, speed=20, forceTurn="None", slowTurnRatio=0.4, correction=0.05, oneWheelTurn="None")
+        _turnToAngle(targetAngle=targetAngle, speed=speed, forceTurn="None", slowTurnRatio=slowturnratio, correction=correction, oneWheelTurn="None")
+        time.sleep(10)
+        targetAngle += increment
 
-    # Left turn zero crossing.
-    turnToAngle(targetAngle = -45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
+        if targetAngle < -175 and increment == -30:
+            targetAngle = -175
+            increment = 30
 
-    # Right turn zero crossing.
-    turnToAngle(targetAngle = 45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
-    """
-    
-    """
-    # Right turn non zero crossing
-    turnToAngle(targetAngle = 45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
+        if targetAngle > 0 and increment == 30:
+            targetAngle = 0
+            increment = -30
 
-    # Right turn non zero crossing
-    turnToAngle(targetAngle = 135, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
+def testCoordinateSystem():
+    for index in range(len(testX2)):
+        robotTest = robot(0,0)
+        robotTest.goto(testX2[index], testY2[index], 0, 10)
 
-    # Right turn non zero crossing
-    turnToAngle(targetAngle = -135, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
 
-    # Right turn non zero crossing
-    turnToAngle(targetAngle = -45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
-
-    # Left turn non zero crossing
-    turnToAngle(targetAngle = -135, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
-
-    # Left turn non zero crossing
-    turnToAngle(targetAngle = 135, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
-
-    # Left turn non zero crossing
-    turnToAngle(targetAngle = 45, speed = 25, forceTurn = "None", slowTurnRatio = 0.4)
-    """
