a996c3a42e5327b376f1e99202d26b10849abe44
RoboticBuilders <rishrao987@gmail.com>
2023-05-14 16:01:46 -0700

    Commiting changes to pathfinder

--- drawing_pathfinder.py
+++ drawing_pathfinder.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from obstacle_avoidance import *
+import obstacle_avoidance
 from time import *
+import time
 
 turtle = Turtle()
 screen = Screen()
@@ -13,11 +15,32 @@ canvas = screen.getcanvas()
 canvas.itemconfig(screen._bgpic, anchor="sw")
 screen.update()
 
+"""
 # Add code now to use the output of obstacle avoidance.
+counter = 0
+while True:
+    if counter % 2 == 0:
+        turtle.pen(fillcolor="black", pencolor="red", pensize=5)
+    else:
+        turtle.pen(fillcolor="black", pencolor="blue", pensize=5)
+    turtle.setposition(30,10)
+    turtle.goto(86, 13.5)
+    turtle.goto(86, 43.5)
+    turtle.goto(190, 20)
+    time.sleep(5000)
+    turtle.clear()
+    counter = counter + 1
+    screen.update()
+"""
 
-turtle.setposition(30,10)
-turtle.goto(86,13.5)
-turtle.goto(86,43.5)
-turtle.goto(190, 20)
+#repeatlyShowThePaths()
+
+# while(True):
+#         findAndShowAllPaths()
+#         sleep(1)
+#         screen.reset()
+#         turtle.pen(fillcolor="black", pencolor="blue", pensize=5)
+
+obstacle_avoidance.repeatlyShowThePaths()
 
 screen.mainloop()
